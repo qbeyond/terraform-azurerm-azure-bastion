45263a2de0ed6dd519af416b1403e763b48166be
Juanjo Dorado <Juan.DoradoRoman@extern.qbeyond.de>
2023-03-23 19:21:35 +0100

    example updated and tested

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,6 +7,13 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
+resource "azurerm_virtual_network" "example" {
+  name                = "examplevnet"
+  address_space       = ["192.168.33.0/24"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
 resource "azurerm_network_security_group" "example" {
   name                = "acceptanceTestSecurityGroup1"
   location            = azurerm_resource_group.example.location
@@ -25,15 +32,15 @@ resource "azurerm_network_security_group" "example" {
   }
 
   tags = {
-    environment = "Production"
+    environment = "dev"
   }
 }
 
 resource "azurerm_subnet" "example" {
-  name                 = "bn-vnet-01"
+  name                 = "AzureBastionSubnet"
   resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["0.0.0.0/26"]
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["192.168.33.0/26"]
 }
 
 module "azureBastion" {
@@ -44,4 +51,5 @@ module "azureBastion" {
   rg_management_name          = "rg-management-01"
   subnet_data                 = azurerm_subnet.example
   network_security_group_data = azurerm_network_security_group.example
+  sku                         = "Standard"
 }
